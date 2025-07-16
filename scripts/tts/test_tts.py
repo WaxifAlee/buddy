5eea50515a22ecd9193f3208671329d226cfb07d
Wasif Ali <wali@zolvat.com>
2025-07-16 22:59:44 +0500

    TTS file added as package

--- scripts/tts/test_tts.py
+++ scripts/tts/test_tts.py
@@ -1,7 +1,9 @@
 import os
 import sys
+
+# Dynamically add root dir to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 
-from tts.tts_engine import speak
+from scripts.tts.tts_engine import speak
 
-speak("Hello Mr. Wasif. I am now able to speak. Let's take over the world.")
+speak("Hello Mr. Wasif. I am now able to speak. Let’s take over the world.")
